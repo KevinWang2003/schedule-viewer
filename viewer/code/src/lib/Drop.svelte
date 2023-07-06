7f75b115ad9e3139b18e58ea10c73daf51f4c664
xTheNightShade (Kevin Wang) <89802545+KevinWang2003@users.noreply.github.com>
2023-07-06 17:09:50 +0200

    Drag Drop functionality

--- viewer/code/src/lib/Drop.svelte
+++ viewer/code/src/lib/Drop.svelte
@@ -1,3 +1,11 @@
+<script type="text/javascript" src="excelToJson.js">
+    function convertExcel(event) {
+      event.preventDefault();
+      const file = document.getElementById('myFile')
+      console.log('click');
+      console.log(file.value)
+    }
+  </script>
 
 <div class="container mx-auto">
     <h1 class="text-4xl">Drop</h1>
@@ -7,8 +15,7 @@
             <input type="file" id="myFile" name="filename">
         </div>
         <div class="mt-5">
-            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+            <button on:click={convertExcel} id="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Submit</button>
         </div>
     </form>
 </div>
-
