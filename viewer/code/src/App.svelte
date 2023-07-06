349bed29f30d5f32bdd0d9e29771640f80455e27
Jessevdbos <bos0038@hz.nl>
2023-07-06 22:22:11 +0200

    background revamp

--- viewer/code/src/App.svelte
+++ viewer/code/src/App.svelte
@@ -6,12 +6,35 @@
 </script>
 
 <Nav />
-<main class="pt-12">
-  <div class="">
-    <Filters />
-  </div>
-
-  <div class="">
-    <Schedule />
+<main id="background" class="pt-12 unselectable">
+  <div style="background-color: rgb(255,255,255,.2);">
+    <div class="">
+      <Filters />
+    </div>
+  
+    <div class="">
+      <Schedule />
+    </div>
   </div>
 </main>
+
+<style>
+  #background {
+        background-image: url("../src/assets/images/pier-lake-hallstatt-austria.jpg");
+
+        /* Create the parallax scrolling effect */
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .unselectable {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+</style>
--- viewer/code/src/lib/components/Week.svelte
+++ viewer/code/src/lib/components/Week.svelte
@@ -6,14 +6,14 @@
         var parent = target.parentElement;
         // var grandparent = parent.parentElement;
         const sibling = target.nextElementSibling;
-        sibling.classList.remove('hidden');
+        sibling.classList.remove("hidden");
         // console.log(sibling);
     };
 
     export let week;
 
     function handleMouseWheel(event) {
-        const container = document.querySelector('.scrollable-container');
+        const container = document.querySelector(".scrollable-container");
         const delta = Math.sign(event.deltaY);
         let scrollAmount = 0;
         if (delta > 0) {
@@ -24,53 +24,95 @@
 
         container.scrollLeft += delta * scrollAmount;
     }
+
+    function handleOnClick(direction) {
+        const container = document.querySelector(".scrollable-container");
+        let scrollAmount = 0;
+        if (direction > 0) {
+            scrollAmount = window.innerWidth + 1; // Adjust the scroll amount as needed
+        } else if (direction < 0) {
+            scrollAmount = -window.innerWidth; // Adjust the scroll amount as needed
+        }
+
+        container.scrollLeft += scrollAmount;
+    }
 </script>
 
-<style>
-    .scrollable-container {
-        overflow-y: hidden;
-        white-space: nowrap;
-        scroll-behavior: auto;
-        max-height: 50vh;
-    }
-
-    .week {
-        display: inline-block;
-        width: 100vw; /* Adjust as needed */
-        white-space: normal;
-    }
-</style>
-
 <div class="week">
-    <div class="text-center mb-20 scrollable-container" on:wheel|preventDefault={handleMouseWheel}>
+    <div
+        class="text-center mb-10 scrollable-container"
+        style="padding: 1%"
+        on:wheel|preventDefault={handleMouseWheel}
+    >
         <p class="text-2xl text-center">Week</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block"  style="position: relative; top: -2vh;" on:click={() => handleOnClick(-1)}>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
         {#if week.weeknr != 36 && week.weeknr != 2}
-            <p class="inline-block p-6 rounded-xl text-center bg-gray-200" style="opacity: 50%;">{week.weeknr - 1}</p>
+            <p
+                class="inline-block p-6 rounded-xl text-center bg-gray-200"
+                style="opacity: 50%; position: relative; top: -2vh;"
+            >
+                {week.weeknr - 1}
+            </p>
         {:else if week.weeknr == 2}
-            <p class="inline-block p-6 rounded-xl text-center bg-gray-200" style="opacity: 50%;">51</p>
+            <p
+                class="inline-block p-6 rounded-xl text-center bg-gray-200"
+                style="opacity: 50%; position: relative; top: -2vh;"
+            >
+                51
+            </p>
         {:else}
-            <p class="inline-block p-6 rounded-xl text-center bg-gray-200" style="opacity: 0%;">{week.weeknr - 1}</p>
+            <p
+                class="inline-block p-6 rounded-xl text-center bg-gray-200"
+                style="opacity: 0%;position: relative; top: -2vh;"
+            >
+                {week.weeknr - 1}
+            </p>
         {/if}
-        <h3 class="text-6xl inline-block p-6 rounded-xl text-center text-cyan-400 bg-gray-200" id={week.weeknr}> {week.weeknr}</h3>
+        <h3
+            class="text-6xl inline-block p-6 rounded-xl text-center text-cyan-400 bg-gray-200"
+            id={week.weeknr}
+        >
+            {week.weeknr}
+        </h3>
         {#if week.weeknr != 5 && week.weeknr != 51}
-            <p class="inline-block p-6 rounded-xl text-center bg-gray-200" style="opacity: 50%;">{week.weeknr + 1}</p>
+            <p
+                class="inline-block p-6 rounded-xl text-center bg-gray-200"
+                style="opacity: 50%; position: relative; top: -2vh;"
+            >
+                {week.weeknr + 1}
+            </p>
         {:else if week.weeknr == 51}
-            <p class="inline-block p-6 rounded-xl text-center bg-gray-200" style="opacity: 50%;">2</p>
+            <p
+                class="inline-block p-6 rounded-xl text-center bg-gray-200"
+                style="opacity: 50%; position: relative; top: -2vh;"
+            >
+                2
+            </p>
         {:else}
-            <p class="inline-block p-6 rounded-xl text-center bg-gray-200" style="opacity: 0%;">{week.weeknr - 1}</p>
+            <p
+                class="inline-block p-6 rounded-xl text-center bg-gray-200"
+                style="opacity: 0%; position: relative; top: -2vh;"
+            >
+                {week.weeknr - 1}
+            </p>
         {/if}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block" style="position: relative; top: -2vh;" on:click={() => handleOnClick(1)}>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
     </div>
     <div class="grid grid-cols-1 md:grid-cols-5 gap-5">
         <div class="mo">
             <h3 class="text-center text-2xl">Monday</h3>
             {#each week.mo as mo}
-                <Lesson lesson={mo} on:mousedown={handleClick}/>
+                <Lesson lesson={mo} on:mousedown={handleClick} />
             {/each}
         </div>
         <div class="tu">
             <h3 class="text-center text-2xl">Tuesday</h3>
             {#each week.tu as tu}
-                <Lesson lesson={tu} on:mousedown={handleClick}/>
+                <Lesson lesson={tu} on:mousedown={handleClick} />
             {/each}
         </div>
         <div class="we">
@@ -82,14 +124,28 @@
         <div class="th">
             <h3 class="text-center text-2xl">Thursday</h3>
             {#each week.th as th}
-                <Lesson lesson={th} on:mousedown={handleClick}/>
+                <Lesson lesson={th} on:mousedown={handleClick} />
             {/each}
         </div>
         <div class="fr">
             <h3 class="text-center text-2xl">Friday</h3>
             {#each week.fr as fr}
-                <Lesson lesson={fr} on:mousedown={handleClick}/>
+                <Lesson lesson={fr} on:mousedown={handleClick} />
             {/each}
         </div>
     </div>
 </div>
+
+<style>
+    .scrollable-container {
+        overflow-y: hidden;
+        scroll-behavior: auto;
+        max-height: 60vh;
+    }
+
+    .week {
+        display: inline-block;
+        width: 100vw; /* Adjust as needed */
+        white-space: normal;
+    }
+</style>
